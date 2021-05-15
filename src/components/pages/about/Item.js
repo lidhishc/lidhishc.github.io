629efc7aaa43fd4a163ef4465e5a772fdca61806
lidhishc <lidhishc@gmail.com>
2021-05-15 22:43:27 +0530

    new1

--- src/components/pages/about/Item.js
+++ src/components/pages/about/Item.js
@@ -1,9 +1,9 @@
 import React,{useState,useEffect} from 'react'
 
 function Item({title,count}) {
+
 const [level1,setLevel1]=useState(null)
     useEffect(()=>{
-     
         let arr1=[]
         for(let i=0;i<count;i++){
             arr1.push(<span style={{color:'orenge',fontWeight:'bolder'}}>* </span>)
@@ -12,7 +12,7 @@ const [level1,setLevel1]=useState(null)
             arr1.push(<span style={{color:'grey'}}>* </span>)
         }
    setLevel1(arr1)
-    },[])
+    },[count])
     return (
         <div className='card'>
             {title}
